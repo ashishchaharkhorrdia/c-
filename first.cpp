b905a7c992c9c4f03347c70c24c3d21f473e1c2c
ashishchaharkhorrdia <achaharkhorrdia@gmail.com>
2026-06-25 19:58:28 +0530

    We learn how to take input and how to use the input data

--- first.cpp
+++ first.cpp
@@ -5,3 +5,8 @@ int main(){
     cout<<"Hello World"<<" Ashish"<<endl;
     return 0;
 }
+
+
+
+//It is a basic C++ program that prints "Hello World Ashish" to the console. 
+//The program includes the necessary header files, uses the standard namespace, and defines the main function where the output is generated.
---
+++ i-o.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+    cout<<"Enter Your Name: ";
+    cin>>name;
+    cout<<"Enter YOur age: ";
+    cin>>age;
+
+    cout<<"Welcome "<<name<<"To our platform"<<"your age is "<<age<<endl;
+    return 0;
+
+
+
+}
