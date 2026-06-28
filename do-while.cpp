954e5341da72dd42121165e3d2367ef59341bb4b
ashishchaharkhorrdia <achaharkhorrdia@gmail.com>
2026-06-28 19:05:14 +0530

    dowhile loop

---
+++ do-while.cpp
@@ -0,0 +1,17 @@
+//do-while loop
+#include<iostream>
+using namespace std;
+int main(){
+    int i ;
+    cout<<"Enter the starting number: ";
+    cin>>i;
+    do{
+        cout<<i<<endl;
+        i++;;
+    }while(i<=10);
+
+
+
+
+    return 0;
+}
